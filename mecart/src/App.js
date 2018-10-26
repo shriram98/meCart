33a2b6173ed967f9298db01dc539c0ed2358cab6
peterjoe3132 <peterjoe3132@gmail.com>
2018-10-26 21:12:03 +0530

    Main Page folder created with Main_Page.js

---
+++ mecart/src/App.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import Main_Page from './Main Page/Main_Page'
+
+class App extends Component{
+    render(){
+        return(
+            
+            <Main_Page />
+        );
+    }
+}
+export default App
---
+++ mecart/src/Main Page/Main_Page.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+
+class Main_Page extends Component{
+    render(){
+        return(
+            <div>
+                  <button >Buy</button>
+                  <button>Sell</button>
+            </div>
+          
+            );
+    }
+}
+export default Main_Page
