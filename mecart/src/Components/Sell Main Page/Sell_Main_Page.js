89453daa2dbdbc0d939a5921a72aead1a5607193
peterjoe3132 <peterjoe3132@gmail.com>
2018-10-30 21:57:00 +0530

    new file:   assets/download.png 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/Components/Sell Main Page/Sell_Main_Page.css 	modified:   src/Components/Sell Main Page/Sell_Main_Page.js 	new file:   src/Components/Sell Main Page/bootstrap.css 	new file:   src/Components/Sell Main Page/bootstrap.min.js 	new file:   src/Components/Sell Main Page/download.png 	modified:   src/index.css 	modified:   src/index.js

--- mecart/src/Components/Sell Main Page/Sell_Main_Page.js
+++ mecart/src/Components/Sell Main Page/Sell_Main_Page.js
@@ -1,10 +1,23 @@
 import React,{Component} from 'react'
-
+import './Sell_Main_Page.css'
+import './bootstrap.css'
+import 'bootstrap'
+// import {collapse} from 'bootstrap'
 class Sell_Main_Page extends Component{
     render(){
         return(
-            <div>
-                <h1>hello</h1>
+            <div className="body">
+                <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="#">
+                <img src="./download.png" className="" alt="" />
+                Mecart
+                </a>
+                </nav>
+                <div className="buttons">
+                    <button type="button" className="btn btn-primary btn-lg new-ad">New Ad </button>
+                    <button type="button" className="btn btn-secondary btn-lg">Edit My Ad</button>
+                </div>
+              
             </div>
         );
     }
