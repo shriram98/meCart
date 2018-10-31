2492980ba2efb08f519d7898c37be1bb3266a55b
Shriram Venugopal <shriramvenugopal123@gmail.com>
2018-11-01 00:56:52 +0530

    Update Sell_Main_Page.js

--- mecart/src/Components/Sell Main Page/Sell_Main_Page.js
+++ mecart/src/Components/Sell Main Page/Sell_Main_Page.js
@@ -1,20 +1,45 @@
 import React,{Component} from 'react'
 import './Sell_Main_Page.css'
 import logo from './logo.png'
+import { Route, Switch, HashRouter } from 'react-router-dom'
+
+import PostAd from ''
+import EditMyAd from ''
 
 class Sell_Main_Page extends Component{
+    constructor(props) {
+        super(props)
+        this.handleClick = this.handleClick.bind(this)
+    }
+    
+    handleClick(e) {
+        if(e.target.id == "button1") {
+            this.props.history.push("/postad")
+        }
+        else {
+            this.props.history.push("/editad")
+        }
+    }    
     render(){
         return(
-         
+                <HashRouter>
                     <div className="body">
                     <div className="buttons">{/* Class  to group the buttons */}
-                    <button type="button" className="btn btn-pri btn-lg new-ad">Post Ad </button>
-                    <button type="button" className="btn btn-sec btn-lg">Edit My Ad</button>
+                    <button type="button" id="button1" className="btn btn-pri btn-lg new-ad" onClick={this.handleClick}>Post Ad </button>
+                    <button type="button" id="button2" className="btn btn-sec btn-lg" onClick={this.handleClick}>Edit My Ad</button>
                     </div>
                     <hr></hr>
                       <img src={logo} width="140" height="140" alt="" className="nav-logo "></img>
                     </div>
-           
+            
+                    <div className="page-content">
+                        <Switch>
+                            <Route path="/postad" component={PostAd} />
+                            <Route path="/editad" component={EditMyAd} />
+                        </Switch>
+                    </div>
+                </HashRouter>
+                
             
         );
     }
