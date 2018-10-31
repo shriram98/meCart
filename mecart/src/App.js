b983e97047e7657fc704bdaaa0cb16473f57c6ea
peterjoe3132 <peterjoe3132@gmail.com>
2018-10-31 20:46:21 +0530

    modified:   src/App.js 	modified:   src/Components/Main Page/Main_Page.css 	modified:   src/Components/Sell Main Page/Sell_Main_Page.css 	modified:   src/Components/Sell Main Page/background.jpg

--- mecart/src/App.js
+++ mecart/src/App.js
@@ -6,7 +6,7 @@ class App extends Component{
     render(){
         return(
             <div>
-                {/* this is the format to call your page  */}
+          
             {/* <Main_Page />  */}
             
             <Sell_Main_Page /> 
